b12a3a81f7e2a124a805b5c643beea088d3630f0
Steven McSorley <smcsorl@sky.com>
2024-06-13 11:26:34 +0100

    add better cards and css

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -10,13 +10,25 @@ const HomePage = () => {
         <ProfileCard
           title="Project One"
           description="This is a brief description of Project One."
-          imageUrl="https://via.placeholder.com/300"
+          imageUrl="https://picsum.photos/200/300?random=1"
           link="#"
         />
         <ProfileCard
           title="Project Two"
           description="This is a brief description of Project Two."
-          imageUrl="https://via.placeholder.com/300"
+          imageUrl="https://picsum.photos/200/300?random=2"
+          link="#"
+        />
+        <ProfileCard
+          title="Project Three"
+          description="This is a brief description of Project Three."
+          imageUrl="https://picsum.photos/200/300?random=3"
+          link="#"
+        />
+        <ProfileCard
+          title="Project Four"
+          description="This is a brief description of Project Four."
+          imageUrl="https://picsum.photos/200/300?random=4"
           link="#"
         />
       </div>
