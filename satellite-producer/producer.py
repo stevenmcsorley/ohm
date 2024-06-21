ee987c4053a8130884c82a6eae626192025e3b2d
Steven McSorley <smcsorl@sky.com>
2024-06-21 12:14:06 +0100

    kafka sats

--- satellite-producer/producer.py
+++ satellite-producer/producer.py
@@ -3,15 +3,37 @@ import json
 import time
 from kafka import KafkaProducer, errors
 import logging
+from datetime import datetime, timedelta
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
+
+# Constants
+API_KEY = 'EP8BLZ-YCNL8Q-PNS2FR-51BF'  # Replace with your actual API key
+BASE_URL = 'https://api.n2yo.com/rest/v1/satellite'
+TOPIC = 'satellite-positions'
+REQUESTS_PER_ROUND = 11  # Number of requests per round
+
+# List of NORAD IDs for the NAVSTAR satellites
+SATELLITE_NORAD_IDS = [
+    45854, 44506, 41019, 40730, 40534, 39533, 39166,
+    32711, 32260, 27704, 27663
+]
+
+# Observer's location
+OBSERVER_LAT = 41.702
+OBSERVER_LNG = -76.014
+OBSERVER_ALT = 0
+SECONDS = 60  # Number of future positions to return
+
+# Rate limiting variables
+RATE_LIMIT = 1000
+ROUNDS_PER_HOUR = RATE_LIMIT // REQUESTS_PER_ROUND
+INTERVAL_BETWEEN_ROUNDS = 3600 / ROUNDS_PER_HOUR
 
 # Function to fetch satellite data
-def fetch_satellite_data():
-    # url = 'https://api.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/2/&apiKey=EP8BLZ-YCNL8Q-PNS2FR-51BF'
-    url = 'https://api.n2yo.com/rest/v1/satellite/positions/24946/41.702/-76.014/0/15/&apiKey=EP8BLZ-YCNL8Q-PNS2FR-51BF'
-
+def fetch_satellite_data(satid):
+    url = f'{BASE_URL}/positions/{satid}/{OBSERVER_LAT}/{OBSERVER_LNG}/{OBSERVER_ALT}/{SECONDS}/&apiKey={API_KEY}'
     response = requests.get(url)
     return response.json()
 
@@ -21,15 +43,18 @@ while not producer:
     try:
         producer = KafkaProducer(bootstrap_servers='kafka:9092',
                                  value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-        logging.info("Kafka producer connected successfully.")
+        # logging.info("Kafka producer connected successfully.")
     except errors.NoBrokersAvailable:
-        logging.error("No Kafka brokers available, retrying in 5 seconds...")
+        # logging.error("No Kafka brokers available, retrying in 5 seconds...")
         time.sleep(5)
 
 # Produce data to Kafka
 while True:
-    satellite_data = fetch_satellite_data()
-    logging.info("Fetched satellite data: %s", satellite_data)
-    producer.send('satellite-positions', satellite_data)
-    logging.info("Data sent to Kafka topic 'satellite-positions'.")
-    time.sleep(10)  # Fetch data every second
+    for i in range(0, len(SATELLITE_NORAD_IDS), REQUESTS_PER_ROUND):
+        current_batch = SATELLITE_NORAD_IDS[i:i + REQUESTS_PER_ROUND]
+        for satid in current_batch:
+            satellite_data = fetch_satellite_data(satid)
+            # logging.info("Fetched satellite data: %s", satellite_data)
+            producer.send(TOPIC, satellite_data)
+            # logging.info("Data sent to Kafka topic '%s'.", TOPIC)
+        time.sleep(INTERVAL_BETWEEN_ROUNDS)
--- src/views/SatelliteTracker.tsx
+++ src/views/SatelliteTracker.tsx
@@ -1,4 +1,23 @@
 import React, { useEffect, useState, useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import "./SatelliteTracker.css";
+
+// Import the satellite icon image
+import satelliteIconUrl from "/public/satellite-icon.png"; // Adjust the path as needed
+
+// Fix the icon issue with Leaflet in React
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let SatelliteIcon = L.icon({
+  iconUrl: satelliteIconUrl,
+  shadowUrl: iconShadow,
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  iconSize: [25, 25], // Adjust the size as needed
+  shadowSize: [41, 41],
+});
 
 interface SatelliteInfo {
   satname: string;
@@ -25,17 +44,17 @@ interface SatelliteDataMap {
 
 const SatelliteTracker: React.FC = () => {
   const [satelliteData, setSatelliteData] = useState<SatelliteDataMap>({});
+  const [updatedKey, setUpdatedKey] = useState<string | null>(null);
   const ws = useRef<WebSocket | null>(null);
 
   const connectWebSocket = () => {
-    const socket = new WebSocket(`ws://websocket.localhost:8080/ws`);
+    const socket = new WebSocket(`ws://websocket.stevenmcsorley.co.uk:8080/ws`);
 
     socket.addEventListener("open", () => {
       console.log("Connected to WS Server");
     });
 
     socket.addEventListener("message", (event) => {
-      console.log("Message from server ", event.data); // Add this line to debug
       try {
         const message = JSON.parse(event.data);
 
@@ -46,8 +65,7 @@ const SatelliteTracker: React.FC = () => {
             newData[message.info.satid] = message;
             return newData;
           });
-        } else {
-          console.warn("Received message with unexpected structure:", message);
+          setUpdatedKey(message.info.satid.toString());
         }
       } catch (error) {
         console.error("Failed to parse JSON", error);
@@ -78,31 +96,94 @@ const SatelliteTracker: React.FC = () => {
     };
   }, []);
 
-  return (
-    <div>
-      <h1>Live Satellite Tracker</h1>
-      {Object.keys(satelliteData).length > 0 ? (
-        <ul>
-          {Object.keys(satelliteData).map((key) => {
-            const data = satelliteData[key];
-            const lastPosition = data.positions[data.positions.length - 1];
+  useEffect(() => {
+    if (updatedKey) {
+      // console.log(`Updated key: ${updatedKey}`);
+      const timeout = setTimeout(() => setUpdatedKey(null), 3000);
+      return () => clearTimeout(timeout);
+    }
+  }, [updatedKey]);
 
-            return (
-              <li key={key}>
-                Satellite: {data.info.satname} <br />
-                Latitude: {lastPosition.satlatitude} <br />
-                Longitude: {lastPosition.satlongitude} <br />
-                Altitude: {lastPosition.sataltitude} km <br />
-                Timestamp:{" "}
-                {new Date(lastPosition.timestamp * 1000).toLocaleString()}{" "}
-                <br />
-                Eclipsed: {lastPosition.eclipsed ? "Yes" : "No"}
-              </li>
-            );
-          })}
-        </ul>
+  return (
+    <div className="container">
+      <h1 className="title">Live Satellite Tracker</h1>
+      {Object.keys(satelliteData).length > 0 ? (
+        <div className="content">
+          <MapContainer center={[0, 0]} zoom={2} className="map-container">
+            <TileLayer
+              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            />
+            {Object.keys(satelliteData).map((key) => {
+              const data = satelliteData[key];
+              const lastPosition = data.positions[data.positions.length - 1];
+
+              return (
+                <Marker
+                  key={key}
+                  position={[
+                    lastPosition.satlatitude,
+                    lastPosition.satlongitude,
+                  ]}
+                  icon={SatelliteIcon}
+                >
+                  <Popup>
+                    <div>
+                      <h2>{data.info.satname}</h2>
+                      <p>
+                        <strong>Latitude:</strong> {lastPosition.satlatitude}
+                      </p>
+                      <p>
+                        <strong>Longitude:</strong> {lastPosition.satlongitude}
+                      </p>
+                      <p>
+                        <strong>Altitude:</strong> {lastPosition.sataltitude} km
+                      </p>
+                      <p>
+                        <strong>Timestamp:</strong>{" "}
+                        {new Date(
+                          lastPosition.timestamp * 1000
+                        ).toLocaleString()}
+                      </p>
+                      <p>
+                        <strong>Eclipsed:</strong>{" "}
+                        {lastPosition.eclipsed ? "Yes" : "No"}
+                      </p>
+                    </div>
+                  </Popup>
+                </Marker>
+              );
+            })}
+          </MapContainer>
+          <div className="list-container">
+            {Object.keys(satelliteData).map((key) => {
+              const data = satelliteData[key];
+              const lastPosition = data.positions[data.positions.length - 1];
+
+              return (
+                <div
+                  className={`card ${updatedKey === key ? "updated" : ""}`}
+                  key={key}
+                >
+                  <h2>{data.info.satname}</h2>
+                  <div className="info">
+                    <p>
+                      <strong>Lat:</strong> {lastPosition.satlatitude}
+                    </p>
+                    <p>
+                      <strong>Long:</strong> {lastPosition.satlongitude}
+                    </p>
+                    <p>
+                      <strong>Alt:</strong> {lastPosition.sataltitude} km
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       ) : (
-        <p>No data available.</p>
+        <p className="no-data">No data available.</p>
       )}
     </div>
   );
